b273d63fcdd9b48b3b939e83d04e0e2119759b67
SeungHee_meatbox <binaryone21@gmail.com>
2022-09-29 12:19:30 +0900

    Sample 정리

---
+++ src/main/java/seunghee/sample/SampleController.java
@@ -0,0 +1,19 @@
+package seunghee.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample/sample1")
+    public String sampleSample1() {
+        return "sample/sample1";
+    }
+
+    @GetMapping("/sample/sample2")
+    public String sampleSample2() {
+        return "sample/sample2";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/sample/sample2.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: meatbox
+  Date: 2022/06/17
+  Time: 2:33 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <div>
+        오잉
+    </div>
+</body>
+</html>
